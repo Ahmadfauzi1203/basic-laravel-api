8c9da0b68755eb7dd938c081da03eb8f01eb529b
Ahmadfauzi1203 <ahmadfauzi12346@gmail.com>
2023-08-31 22:58:14 +0700

    push project laravel-api

---
+++ app/Http/Controllers/Api/BukuController.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Buku;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class BukuController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $data = Buku::orderBy('judul', 'asc')->get();
+        return response()->json(
+            [
+
+                'status' => true,
+                'message' => 'Data Ditemukan',
+                'data' => $data
+            ],
+            200
+        );
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $dataBuku = new Buku;
+
+        $rules = [
+            'judul' => 'required',
+            'pengarang' => 'required',
+            'tgl_publikasi' => 'required|date',
+        ];
+        $validator = Validator::make($request->all(), $rules);
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Gagal memasukkan data',
+                'data' => $validator->errors()
+            ]);
+        }
+
+        $dataBuku->judul = $request->judul;
+        $dataBuku->pengarang = $request->pengarang;
+        $dataBuku->tgl_publikasi = $request->tgl_publikasi;
+
+        $post = $dataBuku->save();
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Sukses memasukkan data'
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $data = Buku::find($id);
+        if ($data) {
+            return response()->json(
+                [
+                    'status' => true,
+                    'message' => 'Data Ditemukan',
+                    'data' => $data
+                ],
+                200
+            );
+        } else {
+            return response()->json(
+                [
+                    'status' => false,
+                    'message' => 'Data Tidak Ditemukan',
+
+                ],
+
+            );
+        }
+    }
+
+    /**
+     * delete the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $dataBuku = Buku::find($id);
+        if (empty($dataBuku)) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Data tidak ditemukan'
+            ], 404);
+        }
+        $rules = [
+            'judul' => 'required',
+            'pengarang' => 'required',
+            'tgl_publikasi' => 'required|date',
+        ];
+        $validator = Validator::make($request->all(), $rules);
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Gagal melakukan update data',
+                'data' => $validator->errors()
+            ]);
+        }
+
+        $dataBuku->judul = $request->judul;
+        $dataBuku->pengarang = $request->pengarang;
+        $dataBuku->tgl_publikasi = $request->tgl_publikasi;
+
+        $post = $dataBuku->save();
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Sukses melakukkan update data'
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $dataBuku = Buku::find($id);
+        if (empty($dataBuku)) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Data tidak ditemukan'
+            ], 404);
+        }
+
+
+        $post = $dataBuku->delete();
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Sukses melakukkan delete data'
+        ]);
+    }
+}
---
+++ app/Models/Buku.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Buku extends Model
+{
+    use HasFactory;
+
+    protected $table = 'buku';
+    protected $fillable = [
+        'judul', 'pengarang', 'tgl_publikasi'
+    ];
+}
